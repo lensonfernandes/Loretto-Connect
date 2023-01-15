ddfb711e4020775083a19216e376d6468dfde6ad
lensonfernandes <lenson.fernand@gmail.com>
2023-01-15 16:46:20 +0530

    Mobile Dropdown menu functionality added

--- lorettoconnect/src/components/Navbar.jsx
+++ lorettoconnect/src/components/Navbar.jsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React , {useState}from "react";
 import { BsPerson } from "react-icons/bs";
 import { BiSearch } from "react-icons/bi";
 import { AiOutlineClose } from "react-icons/ai";
@@ -6,6 +6,13 @@ import { HiOutlineMenuAlt4 } from "react-icons/hi";
 import {FaFacebook, FaTwitter, FaYoutube,FaPinterest, FaInstagram } from 'react-icons/fa'
 
 const Navbar = () => {
+
+const [nav, setNav] = useState(false);
+
+let handleNav = () => {
+  setNav(!nav)
+}
+
   return (
     <div className="flex justify-between items-center h-20 px-4">
       <div>
@@ -23,23 +30,24 @@ const Navbar = () => {
       </div>
 
 {/* Hamburger for mobile  */}
-      <div className="block md:hidden">
-        <HiOutlineMenuAlt4 size={20}/>
+      <div className=" md:hidden" onClick={handleNav}>
+        {nav ? <AiOutlineClose size={20} /> :<HiOutlineMenuAlt4 size={20}/> }
+        
       </div>
 
 
-      <div className="absolute left-0 top-0 w-full bg-gray-200/90 px-4 py-7 flex flex-col">
+      <div onClick={handleNav} className={nav ? 'absolute left-[0] top-0 w-full bg-gray-200/90 px-4 py-7 flex flex-col':'absolute left-[-100%] top-0 w-full bg-gray-200/90 px-4 py-7 flex flex-col'}>
       <ul >
         <h1>Loretto.Connect</h1>
-        <li>Home</li>
-        <li>Upcoming Events</li>
-        <li>Announcements</li>
-        <li>Gallery</li>
-        <div>
-          <button>LogIn</button>
+        <li className='border-b'>Home</li>
+        <li className='border-b'>Upcoming Events</li>
+        <li className='border-b'>Announcements</li>
+        <li className='border-b'>Gallery</li>
+        <div className='flex flex-col'>
+          <button className='my-6'>LogIn</button>
           <button>About</button>
         </div>
-        <div>
+        <div className="flex justify-between my-6">
           <FaFacebook className='icon' />
           <FaTwitter  className='icon'/>
           <FaYoutube className='icon' />
