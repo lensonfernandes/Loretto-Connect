5e31e2b33f0343e4c55696d9a63947c04d915e89
lensonfernandes <lenson.fernand@gmail.com>
2023-01-17 17:46:13 +0530

    Updated content of Memories

--- lorettoconnect/src/components/Memories.jsx
+++ lorettoconnect/src/components/Memories.jsx
@@ -1,7 +1,7 @@
 import React from 'react'
 
-import infant1 from '../assets/infant1.jpg'
-import infant2 from '../assets/infant2.jpg'
+import oath1 from '../assets/oath-1.jpg'
+import Thanks1 from '../assets/thanks1.jpg'
 import infant3 from '../assets/infant3.jpg'
 import infant4 from '../assets/infant4.jpg'
 import infant5 from '../assets/infant5.jpg'
@@ -10,16 +10,19 @@ import MemoriesCard from './MemoriesCard'
 
 const Memories = () => {
   return (
+    <>
+    <h2 className='max-w-[1240px] mx-auto px-4 '>Memories</h2>
     <div className='max-w-[1240px] mx-auto px-4 py-16 grid sm:grid-cols-2 lg:grid-cols-3 gap-4 '>
-        <MemoriesCard bg={infant1} />
-        <MemoriesCard bg={infant2} />
+        <MemoriesCard bg={oath1} desc={"New Parish Council"} />
+        <MemoriesCard bg={Thanks1} desc={"Thanksgiving Adoration"} />
         <MemoriesCard bg={infant3} />
         <MemoriesCard bg={infant4} />
         <MemoriesCard bg={infant5} />
         <MemoriesCard bg={infant6} />
 
         
-    </div>
+    </div></>
+    
   )
 }
 
--- lorettoconnect/src/components/MemoriesCard.jsx
+++ lorettoconnect/src/components/MemoriesCard.jsx
@@ -5,7 +5,7 @@ function MemoriesCard(props) {
     <div className='relative'>
     <img src={props.bg} alt=""  className=" w-full h-full object-cover"/>
     <div className='bg-gray-900/30 absolute top-0 left-0 w-full h-full'>
-        <p className='left-4 bottom-4 text-2xl text-white font-bold absolute '>Infant 1</p>
+        <p className='left-4 bottom-4 text-2xl text-white font-bold absolute '>{props.desc}</p>
     </div>
     </div>
    
