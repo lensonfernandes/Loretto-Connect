ad83befe2de9d39e760b2ceb4240fd61b0b5f329
lensonfernandes <lenson.fernand@gmail.com>
2023-01-18 16:36:24 +0530

    Updated content of Memories

--- lorettoconnect/src/components/Memories.jsx
+++ lorettoconnect/src/components/Memories.jsx
@@ -2,10 +2,10 @@ import React from 'react'
 
 import oath1 from '../assets/oath-1.jpg'
 import Thanks1 from '../assets/thanks1.jpg'
-import infant3 from '../assets/infant3.jpg'
-import infant4 from '../assets/infant4.jpg'
-import infant5 from '../assets/infant5.jpg'
-import infant6 from '../assets/infant6.jpg'
+import LorethmaiFeast from '../assets/loretMaiFeast.jpg'
+import Children from '../assets/children.jpg'
+import GoGoa from '../assets/goGoa.jpg'
+import Environment from '../assets/environment.jpg'
 import MemoriesCard from './MemoriesCard'
 
 const Memories = () => {
@@ -15,10 +15,10 @@ const Memories = () => {
     <div className='max-w-[1240px] mx-auto px-4 py-16 grid sm:grid-cols-2 lg:grid-cols-3 gap-4 '>
         <MemoriesCard bg={oath1} desc={"New Parish Council"} />
         <MemoriesCard bg={Thanks1} desc={"Thanksgiving Adoration"} />
-        <MemoriesCard bg={infant3} />
-        <MemoriesCard bg={infant4} />
-        <MemoriesCard bg={infant5} />
-        <MemoriesCard bg={infant6} />
+        <MemoriesCard bg={LorethmaiFeast} desc={"Lorethmai Feast"} />
+        <MemoriesCard bg={Children} desc={"Children's Day"} />
+        <MemoriesCard bg={GoGoa} desc={"Go Goa"}  />
+        <MemoriesCard bg={Environment}  desc={"Environment Day"}/>
 
         
     </div></>
